6dc2e1420ab67f9726b5d1739a4e4942adf20e00
Josh West <trademasterwong@gmail.com>
2022-04-27 09:50:17 -0500

    add fight round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,43 @@
 //this creates a function named "fight"
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 11;
+var playerAttack= 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(10+10);
+var enemyName = "Roborto";
+var enemyHealth = 10;
+var enemyAttack = 12;
 
-console.log("Our robot's name is " + playerName);
-
-function fight(){
-    window.alert("The fight has begun!")
+var fight = function () {
+// Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+// subtract playerAttack from enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+// log results to console
+    console.log(
+        playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + "health remaining.");
+// enemy health check
+if (enemyHealth<= 0){
+    window.alert(enemyName + " has died!");
 }
-// fight();
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+//Subtract enemyAttack from playerHealth
+    playerHealth = playerHealth - enemyAttack;
+//log results to console
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " ."
+    );
+//player health check
+if (enemyHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+    };
+
+fight();
