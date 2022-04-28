95e2affc70b9a739f99a7558f254eb8c0e1b0966
Josh West <trademasterwong@gmail.com>
2022-04-28 13:16:49 -0500

    initial game finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,16 +73,45 @@ var fight = function (enemyName) {
 
 };
 
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
 
-for (var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth> 0){
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+        else {
+         window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
     }
-    else{
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    endGame(); 
 }
+
+var endGame = function(){
+    window.alert("the game has now ended. Let us see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you have survived the game! You now have a score of " + playerMoney + ".")
+    }
+    else {
+        window.alert("You have lost your robot in battle!");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if(playAgainConfirm){
+        //restart game
+        startGame();
+    }
+
+    else{
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+}
+//Start Game on page load
+startGame();
