5262f97cfbff5a48f5375295b754a80ec4bdd612
Josh West <trademasterwong@gmail.com>
2022-04-28 10:54:32 -0500

    developing gameplay

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,10 +73,15 @@ var fight = function (enemyName) {
 
 };
 
-if (playerHealth> 0){
-    window.alert("Welcome to Robot Gladiators! Round ");
-}
+
 for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth> 0){
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+    }
+    else{
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
